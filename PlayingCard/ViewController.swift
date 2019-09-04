1c6667f7d2d755362e244221d04a3b025c12fbf9
Emma Walker - TVandMobile Platforms - Core Engineering <walkee28@mc-n358038.local>
2019-09-04 15:33:03 +0100

    first commit

---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 04/09/2019.
+//  Copyright © 2019 Emma Walker - TVandMobile Platforms - Core Engineering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Stanford University (CS193P): Developing iOS 11 Apps with Swift 4
+
+<img src="https://is3-ssl.mzstatic.com/image/thumb/course/CobaltPublic128/v4/81/d0/9c/81d09ca1-ec7b-19b2-a215-4ae39df215c5/source/168x214.png" width="100">
+
+This repo keeps track of my progress following Stanford's CS193P course (resources dated 2017-18).
+
+## PlayingCard
+
+### Project 3: 
+
+Created in Xcode 10
